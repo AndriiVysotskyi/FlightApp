a6dbdcd7fa32c522f69be89f88d317acad57b924
Vall <vaillant1989@gmail.com>
2018-12-22 17:15:22 +0200

    initial commit

---
+++ src/test/java/com/flightapp/flightapp/FlightappApplicationTests.java
@@ -0,0 +1,17 @@
+package com.flightapp.flightapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlightappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
